1714607d55449e5b3ea81252d242e730fd81fba0
pzeleznjak <petar.zeleznjak@outlook.com>
2025-11-15 16:07:04 +0100

    added Remove-EKVStore cmdlet

---
+++ EncryptedKeyValueStore/source/Remove-EKVStore.ps1
@@ -0,0 +1,81 @@
+function Remove-EKVStore {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, Position=0, HelpMessage="Name of the new Encrypted Key-Value store to be created")]
+        [string] $Name,
+
+        [Parameter(Mandatory=$true, Position=1, HelpMessage="Master Password of the new Encrypted Key-Value store")]
+        [securestring] $Password
+    )
+
+    $DirectoryPath = Join-Path $PSScriptRoot ".ekvs" 
+    $StorePath = Join-Path $DirectoryPath "$($Name).ekv"
+    if (-Not (Test-Path -Path $StorePath)) {
+        Write-Error "Encrypted Key-Value store $Name does not exist"
+        return $null
+    }
+
+    $FirstLineSplit = (Get-Content -Path $StorePath -TotalCount 1 -Encoding UTF8) -split "\s+"
+    $PasswordSaltHash = $FirstLineSplit[0]
+    $PasswordSalt = $FirstLineSplit[1]
+    
+    $Ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password)
+    try {
+        $PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($Ptr)
+    }
+    finally {
+        [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($Ptr)
+    }
+    $SaltedPassword = $PlainPassword + $PasswordSalt
+    $Bytes = [System.Text.Encoding]::UTF8.GetBytes($SaltedPassword)
+    $SHA256 = [System.Security.Cryptography.SHA256]::Create()
+    $HashBytes = $SHA256.ComputeHash($Bytes)
+    $HashText = ([System.BitConverter]::ToString($HashBytes) -replace "-", "")
+    if ($HashText -ne $PasswordSaltHash) {
+        Write-Error "Invalid Key-Value store Master Password"
+        return $null
+    }
+
+    Write-Host "Are you sure you want to continue? (y/n)" -ForegroundColor Red
+    $answer = Read-Host
+
+    if ($answer -notmatch '^[Yy]') {
+        Write-Host "Operation cancelled."
+        return $null
+    }
+
+    $Records = foreach ($Line in (Get-Content $StorePath | Select-Object -Skip 1)) {
+        $Parts = $Line -split "\s+"
+        $Key = $Parts[0]
+        $EncryptedValueHex = $Parts[1]
+
+        $EncryptedValueBytes = for ($i = 0; $i -lt $EncryptedValueHex.Length; $i += 2) { [Convert]::ToByte($EncryptedValueHex.Substring($i,2),16) }
+
+        $Kdf = [System.Security.Cryptography.Rfc2898DeriveBytes]::new(
+            [System.Text.Encoding]::UTF8.GetBytes($PlainPassword), 
+            [System.Text.Encoding]::UTF8.GetBytes($PasswordSalt), 
+            8192, 
+            [System.Security.Cryptography.HashAlgorithmName]::SHA256)
+        
+        $EncryptionKey = $Kdf.GetBytes(32)
+        $EncryptionIv = $Kdf.GetBytes(16)
+
+        $Aes = [System.Security.Cryptography.Aes]::Create()
+        $Aes.KeySize = 256
+        $Aes.Mode = [System.Security.Cryptography.CipherMode]::CBC
+        $Aes.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
+        $Aes.Key = $EncryptionKey
+        $Aes.Iv = $EncryptionIV
+
+        $Decryptor = $Aes.CreateDecryptor()
+        $DecryptedBytes = $Decryptor.TransformFinalBlock($EncryptedValueBytes, 0, $EncryptedValueBytes.Length)
+        $DecryptedValueText = [System.Text.Encoding]::UTF8.GetString($DecryptedBytes)
+
+        $Key, $DecryptedValueText
+    }
+
+    Remove-Item $StorePath -Force
+    Write-Host "Removed Encrypted Key-Value store $Name"
+
+    return $Records
+}
