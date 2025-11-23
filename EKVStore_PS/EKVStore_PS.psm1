f00ff7ce8e6a0920a374ff859797d594337b57a8
pzeleznjak <petar.zeleznjak@outlook.com>
2025-11-23 16:33:59 +0100

    upgraded EKVStore_PS.psm1 to Unblock all .ps1 files before loading

--- EKVStore_PS/EKVStore_PS.psm1
+++ EKVStore_PS/EKVStore_PS.psm1
@@ -1,5 +1,7 @@
 $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
+Get-ChildItem $ScriptDir -Recurse -Filter *.ps1 -File | Unblock-File
+
 Get-ChildItem "$ScriptDir\source\*.ps1" | ForEach-Object {
     . $_.FullName
     Write-Host "Loaded command: $($_.NameString)"
