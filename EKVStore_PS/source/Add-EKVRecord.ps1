d73c4a45960c55b7826da397e27979fbe556222b
pzeleznjak <petar.zeleznjak@outlook.com>
2025-11-27 16:13:51 +0100

    added RawValue input parameter to Add-EKVRecord and modified Valueinput parameter to be SecureString

--- EKVStore_PS/source/Add-EKVRecord.ps1
+++ EKVStore_PS/source/Add-EKVRecord.ps1
@@ -1,5 +1,5 @@
 function Add-EKVRecord {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = "ByValue")]
     param (
         [Parameter(Mandatory=$true, Position=0, HelpMessage="Name of the Encrypted Key-Value store to access")]
         [string] $Name,
@@ -10,8 +10,11 @@ function Add-EKVRecord {
         [Parameter(Mandatory=$true, Position=2, HelpMessage="Key of the Encrypted Key-Value record to add")]
         [string] $Key,
 
-        [Parameter(Mandatory=$true, Position=3, HelpMessage="Value of the Encrypted Key-Value record to add")]
-        [string] $Value
+        [Parameter(Mandatory=$true, Position=3, ParameterSetName="ByValue", HelpMessage="Secure String Value of the Encrypted Key-Value record to add")]
+        [securestring] $Value,
+
+        [Parameter(Mandatory=$true, Position=4, ParameterSetName="ByRawValue", HelpMessage="Raw value of the Encrypted Key-Value record to add")]
+        [string] $RawValue
     )
 
     $storePath = Get-StorePath -Name $Name -CheckExists
@@ -22,7 +25,11 @@ function Add-EKVRecord {
     $success = Compare-PasswordHashes -MasterPasswordHash $masterPassword.PasswordHash -Password $Password -Salt $masterPassword.Salt
     if (-not $success) { return $null }
 
-    $valueBytes = [System.Text.Encoding]::UTF8.GetBytes($Value)
+    if ($PSBoundParameters.ContainsKey("Value")) {
+        $RawValue = ConvertTo-PlainString -Secure $Value
+    }
+
+    $valueBytes = [System.Text.Encoding]::UTF8.GetBytes($RawValue)
     
     try {
         $aes = New-AESObject -Password $Password -Salt $masterPassword.Salt    
--- todo.md
+++ todo.md
@@ -1,22 +1,21 @@
 # EKV STORE
 ## TO DO
 
-1. Protect all input values with secure string or prompt with Read-Host -AsSecureString
-2. Add key validation - must not contain whitespace and ','
-3. Add Export-ToUnprotectedFile
+1. Add key validation - must not contain whitespace and ','
+2. Add Export-ToUnprotectedFile
     - decrypts all records from a store and saves them in a provided .kv file
-4. Add Import-FromUnprotectedFile
+3. Add Import-FromUnprotectedFile
     - creates a new store and stores all records from provided .kv file
-5. Edit Get-Help to display as much help as possible
-6. Create new Release v1.1.0
-7. Disable duplicate key insertion
+4. Edit Get-Help to display as much help as possible
+5. Create new Release v1.1.0
+6. Disable duplicate key insertion
     - key list after master password - key names are separated by ','
     - each value is in its own line
     - index of key in key list is index of value
-8. Enable record grouping
+7. Enable record grouping
     - group test_account contains test_user and test_password
     - group definitions are in the beginning of .ekv after master password and key list
-9. Write README.md
-10. Edit .psm1 file to include all needed fields
-11. Add all cmdlet metadata/headers/comments/descriptions
-12. Implement in C#
+8. Write README.md
+9. Edit .psm1 file to include all needed fields
+10. Add all cmdlet metadata/headers/comments/descriptions
+11. Implement in C#
