1d91227ae814f1832716e0448fde8986cd41139a
pzeleznjak <petar.zeleznjak@outlook.com>
2025-12-16 21:09:44 +0100

    created a test Get-HelloWorld Cmdlet

---
+++ EKVStore/EKVStore/HelloWorld/HelloWorldServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKVStore.HelloWorld
+{
+    internal static class HelloWorldServices
+    {
+        public static IGreeterService Greeter { get; set; } = new GreeterService();
+    }
+}
