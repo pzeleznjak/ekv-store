3551ba4f5ab7fb4d998bee9d09ce6f1d8f6422c1
pzeleznjak <petar.zeleznjak@outlook.com>
2026-04-07 22:02:19 +0200

    added GetEKVStoresCmdlet

---
+++ EKVStore/EKVStore/Cmdlets/GetEKVStoresCmdlet.cs
@@ -0,0 +1,26 @@
+﻿using EKVStore.Initialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKVStore.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "EKVStores")]
+    public class GetEKVStoresCmdlet : AbstractEKVCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            List<string> stores = [];
+            string directoryPath = EKVModuleContext.GetStoreDirectory();
+            foreach (string file in Directory.GetFiles(directoryPath, "*.ekv"))
+            {
+                string baseName = Path.GetFileNameWithoutExtension(file);
+                stores.Add(baseName);
+            }
+            WriteObject(stores);
+        }
+    }
+}
