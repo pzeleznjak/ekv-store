35a728c8bde9d4d23db23f6f5b460ec6a1ebd17e
pzeleznjak <petar.zeleznjak@outlook.com>
2026-04-19 11:25:05 +0200

    added TestEKVPasswordCmdlet

---
+++ EKVStore/EKVStore/Cmdlets/TestEKVPasswordCmdlet.cs
@@ -0,0 +1,46 @@
+﻿using EKVStore.Initialization;
+using EKVStore.Utils;
+using EKVStore.Utils.ArgumentCompleters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKVStore.Cmdlets
+{
+    [Cmdlet(VerbsDiagnostic.Test, "EKVPassword")]
+    public class TestEKVPasswordCmdlet : AbstractEKVCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, HelpMessage = "Name of the Encrypted Key-Value store to access")]
+        [ArgumentCompleter(typeof(EKVStoreNameArgumentCompleter))]
+        public required string Name { get; set; }
+
+        [Parameter(Mandatory = true, Position = 1, HelpMessage = "Master Password of the Encrypted Key-Value store to test")]
+        public required SecureString Password { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            string storeFile = EKVModuleContext.GetStoreFile(Name);
+            if (!File.Exists(storeFile))
+            {
+                WriteError(new ErrorRecord(new FileNotFoundException($"Encrypted Key-Value store {Name} does not exist"), "EKVStoreDoesNotExist", ErrorCategory.ObjectNotFound, this));
+                WriteObject(false);
+                return;
+            }
+
+            MasterPassword masterPassword = MasterPassword.ReadMasterPassword(storeFile);
+            if (!masterPassword.ComparePasswordHash(Password))
+            {
+                Host.UI.WriteLine(ConsoleColor.Red, Host.UI.RawUI.BackgroundColor, $"Given password is not master password of the {Name} EKV store");
+                WriteObject(false);
+                return;
+            }
+
+            Host.UI.WriteLine(ConsoleColor.Green, Host.UI.RawUI.BackgroundColor, $"Given password is master password of the {Name} EKV store");
+            WriteObject(true);
+        }
+    }
+}
